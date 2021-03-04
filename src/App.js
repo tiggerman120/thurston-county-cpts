f8230abc7ab5345eee0f52ef09358e03a6549685
garrett <grrttcintron@gmail.com>
2021-03-04 15:23:31 -0800

    header component rabbit holed. reset commiting for deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from '@material-ui/core'
+import Header from './components/header/header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+    <Header />
+    <h1> app proof of life</h1>
+    </Container>
   );
 }
 
